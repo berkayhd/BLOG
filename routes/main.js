a7510773bb8fdb4e029cc1b399c16582a7b128df
Berkay İleri <brkyilri1453@gmail.com>
2021-04-13 00:50:17 +0300

    post ekle butonuna basıldıgında yönlendirme yaptık

--- routes/main.js
+++ routes/main.js
@@ -37,4 +37,8 @@ router.get('*', function (req, res) {
     res.status(404).send('Not Found')
 })
 
+router.post('/posts/test', function (req, res) {
+    res.redirect('/')
+})
+
 module.exports = router
